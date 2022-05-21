4994e810d6d5b5107518e7941e915d2ab5115bc9
Daniel Soto <dasg.1993@gmail.com>
2022-05-21 12:00:29 -0500

    [topic] - add states & list of task

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 // import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { themes } from "./src/constants/themes/index";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const onHandleInput = (text) => {
+    setTask(text);
+  };
+
+  const onHandleSubmit = () => {
+    setTask("");
+    setTasks([...tasks, task]);
+  };
+
+  console.warn({ tasks });
+
   return (
     <View style={themes.container}>
       <View style={styles.containerTask}>
-        <TextInput placeholder="add new task" />
-        <Button title="ADD" color="#8CBCB9" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="add new task"
+          value={task}
+          onChangeText={onHandleInput}
+        />
+        <Button title="ADD" color="#8CBCB9" onPress={() => onHandleSubmit()} />
+      </View>
+      <View style={styles.containerList}>
+        {tasks.map((item, idx) => (
+          <View key={`task-${idx}`} style={styles.containerItem}>
+            <Text style={styles.item}>{item}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -15,9 +42,30 @@ export default function App() {
 
 const styles = StyleSheet.create({
   containerTask: {
+    padding: 25,
     flex: 1,
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     alignItems: "center",
+  },
+  textInput: {
+    borderColor: "#8CBCB9",
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    width: "60%",
+    height: 40,
+    fontSize: 14,
+    color: "#212121",
+  },
+  containerList: {
+    margin: 25,
+  },
+  containerItem: {
+    padding: 10,
+  },
+  item: {
+    fontSize: 14,
+    color: "#212121",
+    fontWeight: "bold",
   },
 });
